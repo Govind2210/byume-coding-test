252976b2e893bdedb0461c7145bf2417d629feed
Govind Lowanshi <85470076+Govind2210@users.noreply.github.com>
2022-12-08 17:01:42 +0530

    Add files via upload

---
+++ Coding.js
@@ -0,0 +1,29 @@
+// let  N = 20, d = 5;
+// // let Arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// //Arr = [4,5,6,7,8,9,10, 1,2,3]
+
+// function clockRoattion(nums , N , d){
+//      let arr = []
+//      let arr2 = []
+//      for(let i = d ; i < nums.length ; i++){
+//         // console.log(nums[i])
+//         if(nums[i] > d){
+//             arr[i] = (nums[i])
+//         }  
+//      }
+//      for(let  i = 0 ; i < nums.length  ; i++){
+//         if(nums[i] <= d){
+//             arr2[i] = (nums[i])
+//         }
+//      }
+//     let joininfArr =  arr.concat(arr2)
+//     console.log(joininfArr)
+// }
+// let myArr = []
+// for(let i = 1 ;i <= N ; i++){
+//     // console.log(i)
+//     myArr.push(i)
+// }
+// // console.log(myArr)
+// clockRoattion(myArr , N , d)
+
